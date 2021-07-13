4abd579ef247f74d9e7b5ac364576275777e9aa7
bhavanar <bhavanar@INT-DELL-NOTE.pjmdistribution.local>
2021-07-12 21:29:02 -0700

    SwapNumber

---
+++ Fibonacci Sequence/src/Java/FibonacciSequence.java
@@ -0,0 +1,23 @@
+package Java;
+
+public class FibonacciSequence {
+
+	
+	public static int fibonacci(int num) {
+		if (num == 0) return 0;
+		if (num <=2) return 1;
+		 int fib = fibonacci(num-1) + fibonacci(num-2);
+		 return fib;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while (true) {
+		System.out.println(fibonacci(i));
+		i++;
+		}
+	}
+
+}
+
